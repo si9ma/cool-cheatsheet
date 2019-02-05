01f48075f54e668321c5f728dbeb2614c43f8e41
si9ma <hellob374@gmail.com>
2019-02-05 14:53:15 +0800

    update add deploy script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A cool cheatsheet list base on Tex, with a better reading experience.
 
 Created at 2019-02-04 13:16:57, happy new year! :tada:
+
+## Test
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Created At Tue Feb 05 2019 2:49:24 PM 
+# deploy generated pdf and image to site branch
+# 
+# Copyright 2019 si9ma <hellob374@gmail.com>
+
